153050adacbaf5e16fd32356a524c9300606c3f2
Yarden Zeron <zeron.yarden@gmail.com>
2023-02-05 17:19:41 +0200

    search bar together with axies

--- src/views/YoutubePlayer.jsx
+++ src/views/YoutubePlayer.jsx
@@ -1,25 +1,17 @@
 import { MainVideo } from "../cmps/MainVideo";
 import { SearchBar } from "../cmps/SearchBar";
-import { VideoList } from "../cmps/VideoList";
 
 export function YoutubePlayer() {
 
     return (
         <>
-<<<<<<< HEAD
-        <h1>From youtube-Player cmp</h1>
-        <SearchBar />
-        {/* <VideoList /> */}
-        <MainVideo />
-=======
             <main className="index-layout">
                 <SearchBar />
                 <div className="videos-content">
-                    <SongList />
+                    {/* <SongList /> */}
                     <MainVideo />
                 </div>
             </main>
->>>>>>> c1b4429db17ac7ca71dd27a173670ac42c6ff029
         </>
     )
 }
