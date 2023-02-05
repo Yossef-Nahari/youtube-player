1083f0915ea8617e0512b22108b6b1f017535068
Yarden Zeron <zeron.yarden@gmail.com>
2023-02-05 16:19:58 +0200

    song list

--- src/cmps/SongList.jsx
+++ src/cmps/SongList.jsx
@@ -3,6 +3,23 @@
 export function SongList() {
 
     return (
-        <h1>Song-list</h1>
+        <section className="song-list">
+            <div className="song">
+                <img src="" alt="" />
+                <div className="details">
+                    <h4>Song name: </h4>
+                    <h4>Song description: </h4>
+                    <h4>Total votes: </h4>
+                </div>
+                <div className="btn-play">
+                    <button className="" >Play</button>
+                    
+
+                </div>
+
+            </div>
+
+
+        </section>
     )
 }
