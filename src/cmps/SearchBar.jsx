2373f60f4892398b2a1ae1eaa01e260b5b672e75
Yarden Zeron <zeron.yarden@gmail.com>
2023-02-05 16:20:35 +0200

    search option

--- src/cmps/SearchBar.jsx
+++ src/cmps/SearchBar.jsx
@@ -1,8 +1,37 @@
 
+import Input from "@mui/material/TextField";
+
+
 
 export function SearchBar() {
 
+    function handleChange(event) {
+        const search=event.target.value
+
+    }
+
     return (
-        <h1>Search-bar</h1>
+        <section className=" full index-layout search-bar">
+            <form className="search-form">
+                <div className="input-text">
+                    <Input
+                        id="search-bar"
+                        placeholder="Search on YouTube"
+                        variant="outlined"
+                        fullWidth
+                        color="warning"
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className="trigger-btn">
+                    <button className="btn-search">
+                        Search
+                    </button>
+
+
+                </div>
+            </form>
+        </section>
     )
 }
