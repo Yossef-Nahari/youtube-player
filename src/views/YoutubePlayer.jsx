9ee149df8763f9120aebddb5a113797aae24880c
Yarden Zeron <zeron.yarden@gmail.com>
2023-02-05 20:16:09 +0200

    main-video

--- src/views/YoutubePlayer.jsx
+++ src/views/YoutubePlayer.jsx
@@ -1,17 +1,26 @@
-import { MainVideo } from "../cmps/MainVideo";
-import { SearchBar } from "../cmps/SearchBar";
+import { useState } from "react";
+
+import { youtubeService } from "../services/youtube-service";
+
+import { MainVideo } from "../cmps/main-video";
+import { SearchBar } from "../cmps/search-bar";
+import { VideoList } from "../cmps/video-list ";
 
 export function YoutubePlayer() {
+    const [currVideo, setCurrVideo] = useState(youtubeService.renderVideos()[0])
+
+    function changeVideo(id) {
+        setCurrVideo(id)
+
+    }
 
     return (
-        <>
-            <main className="index-layout">
-                <SearchBar />
-                <div className="videos-content">
-                    {/* <VideoList /> */}
-                    <MainVideo />
-                </div>
-            </main>
-        </>
+        <main className="index-layout">
+            <SearchBar />
+            <div className="videos-content">
+                <VideoList changeVideo={changeVideo} />
+                <MainVideo currVideo={currVideo} />
+            </div>
+        </main>
     )
 }
